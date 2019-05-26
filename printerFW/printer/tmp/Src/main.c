657ccc85c2c66b9f658c45a0f6c6a93f7678dffd
Jan--Henrik <janhenrik@janhenrik.org>
2019-05-26 03:00:00 +0200

    code finally works

--- printerFW/printer/tmp/Src/main.c
+++ printerFW/printer/tmp/Src/main.c
@@ -13,7 +13,7 @@ DMA_HandleTypeDef hdma_usart3_rx;
 DMA_HandleTypeDef hdma_usart3_tx;
 DMA_HandleTypeDef hdma_tim2_uev;
 
-uint16_t PER = 30;
+uint16_t PER = 90;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -24,6 +24,7 @@ static void MX_USART3_UART_Init(void);
 void transmit_error_handler(DMA_HandleTypeDef * hdma);
 void data_tramsmitted_handler(DMA_HandleTypeDef * hdma);
 void dma_init();
+void startDMA(void);
 
 void dfu_otter_bootloader(void)
 {
@@ -31,26 +32,21 @@ void dfu_otter_bootloader(void)
   NVIC_SystemReset();
 }
 
-#define data_len 200
+#define data_len ((208*2))
 uint16_t my_data_buf[data_len];
-
-uint8_t my_data_buf_fire[data_len];
 
 void dataTransmittedHandler0(DMA_HandleTypeDef *hdma) {
   HAL_GPIO_WritePin(GPIOB, LED_STATUS_Pin, 0);
   HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
 }
 
-void dataTransmittedHandler1(DMA_HandleTypeDef *hdma) {
+void dataTransmittedHandler2(DMA_HandleTypeDef *hdma) {
   HAL_GPIO_WritePin(GPIOB, LED_POWER_Pin, 0);
-  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_4);
   HAL_GPIO_WritePin(GPIOC, F5_Pin, 0);
   HAL_GPIO_WritePin(GPIOC, F3_Pin, 0);
 }
 
-void dataTransmittedHandler2(DMA_HandleTypeDef *hdma) {
-  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_4);
-}
 
 int main(void)
 {
@@ -72,35 +68,57 @@ int main(void)
 
   HAL_GPIO_WritePin(GPIOB, LED_POWER_Pin, 1);
 
-  memset(my_data_buf, 0xFF, data_len);
-  for (uint16_t i = 0; i < data_len - 1; i += 8) {
-    my_data_buf[i + 0] = 0xFF83;
-    my_data_buf[i + 1] = 0xFF00;
-    my_data_buf[i + 2] = 0x0005;
-    my_data_buf[i + 3] = 0xFF00;
-    my_data_buf[i + 4] = 0x0009;
-    my_data_buf[i + 5] = 0x0000;
-    my_data_buf[i + 6] = 0xFF41;
-    my_data_buf[i + 7] = 0x0000;
+  memset(my_data_buf, 0xFFFF, data_len*2);
+  for (uint16_t i = 0; i < data_len - 9; i += 16) {
+    my_data_buf[i + 0]  = 0b0000010000000000;
+    my_data_buf[i + 1]  = 0b0110000000100001;
+    my_data_buf[i + 2]  = 0b0110000000100000;
+    my_data_buf[i + 3]  = 0b0110000000001001;
+    my_data_buf[i + 4]  = 0b0110010000001000;
+    my_data_buf[i + 5]  = 0b0110000000000011;
+    my_data_buf[i + 6]  = 0b0110000010000010;
+    my_data_buf[i + 7]  = 0b0110000010010001;
+    my_data_buf[i + 8]  = 0b0110000010010000;
+    my_data_buf[i + 9]  = 0b0000000000000100;
+    my_data_buf[i + 10] = 0b0000000001000100;
+    my_data_buf[i + 11] = 0b0000000001000000;
+    my_data_buf[i + 12] = 0b0000000001000000;
+    my_data_buf[i + 13] = 0b0000000000000000;
+    my_data_buf[i + 14] = 0b0000000000000000;
+    my_data_buf[i + 15] = 0b0000000000000000;
   }
 
-  memset(my_data_buf, 0x00, data_len);
-  for (uint16_t i = 0; i < data_len - 1; i += 8) {
-    my_data_buf_fire[i + 0] = 0x02;//02
-    my_data_buf_fire[i + 1] = 0x00;
-    my_data_buf_fire[i + 2] = 0x00;
-    my_data_buf_fire[i + 3] = 0x00;
-    my_data_buf_fire[i + 4] = 0x01;//01
-    my_data_buf_fire[i + 5] = 0x00;
-    my_data_buf_fire[i + 6] = 0x00;
-    my_data_buf_fire[i + 7] = 0x00;
-  }
+  /*
+  TUT
+  my_data_buf[i + 0]  = 0b0000010000000000;
+  my_data_buf[i + 1]  = 0b0110000000100001;
+  my_data_buf[i + 2]  = 0b1010010000100000;
+  my_data_buf[i + 3]  = 0b0110000000001001;
+  my_data_buf[i + 4]  = 0b1100000000001000;
+  my_data_buf[i + 5]  = 0b1010000000000011;
+  my_data_buf[i + 6]  = 0b0110000010000010;
+  my_data_buf[i + 7]  = 0b0000000010010001;
+  my_data_buf[i + 8]  = 0b1110000010010000;
+  my_data_buf[i + 9]  = 0b0000000000000100;
+  my_data_buf[i + 10] = 0b0000000001000100;
+  my_data_buf[i + 11] = 0b0000000001000000;
+  my_data_buf[i + 12] = 0b0000000001000000;
+  my_data_buf[i + 13] = 0b0000000000000000;
+  my_data_buf[i + 14] = 0b0000000000000000;
+  my_data_buf[i + 15] = 0b0000000000000000;
+  */
+
+  my_data_buf[data_len - 8 + 0] = 0b0000010000000000;//02
+  my_data_buf[data_len - 8 + 1] = 0b0000000000000000;
+  my_data_buf[data_len - 8 + 2] = 0b0000010000000000;
+  my_data_buf[data_len - 8 + 3] = 0b0000000000000000;
+  my_data_buf[data_len - 8 + 4] = 0b0000010000000000;//01
+  my_data_buf[data_len - 8 + 5] = 0b0000000000000000;
+  my_data_buf[data_len - 8 + 6] = 0b0000010000000000;
+  my_data_buf[data_len - 8 + 7] = 0b0000000000000000;
 
   htim2.hdma[TIM_DMA_ID_CC1]->XferCpltCallback = dataTransmittedHandler0;
   __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
-
-  htim2.hdma[TIM_DMA_ID_CC3]->XferCpltCallback = dataTransmittedHandler1;
-  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC3);
 
   htim2.hdma[TIM_DMA_ID_CC4]->XferCpltCallback = dataTransmittedHandler2;
   __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC4);
@@ -111,32 +129,32 @@ int main(void)
     if(HAL_GPIO_ReadPin(GPIOB, BUTTON_Pin)) {
       HAL_GPIO_WritePin(GPIOB, LED_STATUS_Pin, 1);
       startDMA();
+      HAL_Delay(10);
     }
   }
 }
-void startDMA() {
+
+void startDMA(void) {
 
 
   if (HAL_DMA_GetState(htim2.hdma[TIM_DMA_ID_CC1]) == HAL_DMA_STATE_BUSY) {
     return;
   }
-  if (HAL_DMA_GetState(htim2.hdma[TIM_DMA_ID_CC3]) == HAL_DMA_STATE_BUSY) {
-    return;
-  }
+
   if (HAL_DMA_GetState(htim2.hdma[TIM_DMA_ID_CC4]) == HAL_DMA_STATE_BUSY) {
     return;
   }
 
+  TIM2->CR1 &= (TIM_CR1_CEN | 0);
 
-  HAL_DMA_Start_IT(htim2.hdma[TIM_DMA_ID_CC1], (uint32_t)my_data_buf, (uint32_t)&GPIOA->ODR, data_len);
-  HAL_DMA_Start_IT(htim2.hdma[TIM_DMA_ID_CC3], (uint32_t)my_data_buf_fire, (uint32_t)&GPIOB->ODR, data_len);
-  HAL_DMA_Start_IT(htim2.hdma[TIM_DMA_ID_CC4], (uint32_t)my_data_buf, (uint32_t)&GPIOC->ODR, data_len);
+  HAL_DMA_Start_IT(htim2.hdma[TIM_DMA_ID_CC1], (uint32_t)my_data_buf,       (uint32_t)&GPIOA->ODR, data_len);
+  HAL_DMA_Start_IT(htim2.hdma[TIM_DMA_ID_CC4], (uint32_t)my_data_buf,       (uint32_t)&GPIOC->ODR, data_len);
 
   TIM2->CNT = 0u;
 
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_ALL);
+
+  TIM2->CR1 &= (TIM_CR1_CEN | 1);
 }
 
 void SystemClock_Config(void)
@@ -190,11 +208,11 @@ static void MX_TIM1_Init(void)
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
   sConfig.IC2Filter = 0;
   HAL_TIM_Encoder_Init(&htim1, &sConfig);
-  
+
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
  HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig);
-  
+
 
 }
 
@@ -214,10 +232,10 @@ static void MX_TIM2_Init(void)
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   HAL_TIM_Base_Init(&htim2);
-  
+
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
   HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
- 
+
   HAL_TIM_PWM_Init(&htim2);
 
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
@@ -225,22 +243,16 @@ static void MX_TIM2_Init(void)
   HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig);
 
   sConfigOC1.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC1.Pulse = PER / 3;
+  sConfigOC1.Pulse = PER / 3 * 2;
   sConfigOC1.OCPolarity = TIM_OCPOLARITY_LOW;
   sConfigOC1.OCFastMode = TIM_OCFAST_DISABLE;
   HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC1, TIM_CHANNEL_1);
- 
+
   sConfigOC4.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC4.Pulse = (PER / 3) * 2;
+  sConfigOC4.Pulse = PER / 3;
   sConfigOC4.OCPolarity = TIM_OCPOLARITY_LOW;
   sConfigOC4.OCFastMode = TIM_OCFAST_DISABLE;
  HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC4, TIM_CHANNEL_4);
-
-  sConfigOC3.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC3.Pulse = PER / 3;
-  sConfigOC3.OCPolarity = TIM_OCPOLARITY_LOW;
-  sConfigOC3.OCFastMode = TIM_OCFAST_DISABLE;
-  HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC3, TIM_CHANNEL_3);
 }
 
 static void MX_USART3_UART_Init(void)
@@ -279,9 +291,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOC, D1_Pin | D2_Pin | D3_Pin, GPIO_PIN_RESET);
 
   HAL_GPIO_WritePin(GPIOA, DCLCK_Pin | S5_Pin | S2_Pin | S3_Pin
-                    | S4_Pin | S1_Pin | LED_STATUSA15_Pin, GPIO_PIN_RESET);
+                    | S4_Pin | S1_Pin | CSYNC_Pin  | F3_Pin | F5_Pin, GPIO_PIN_RESET);
 
-  HAL_GPIO_WritePin(GPIOB, LED_STATUS_Pin | LED_POWER_Pin | CSYNC_Pin  | F3_Pin | F5_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LED_STATUS_Pin | LED_POWER_Pin , GPIO_PIN_RESET);
 
   GPIO_InitStruct.Pin = D1_Pin | D2_Pin | D3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -290,7 +302,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   GPIO_InitStruct.Pin = DCLCK_Pin | S5_Pin | S2_Pin | S3_Pin
-                        | S4_Pin | S1_Pin;
+                        | S4_Pin | S1_Pin | F3_Pin | F5_Pin | CSYNC_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
@@ -301,7 +313,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(INT_IN_GPIO_Port, &GPIO_InitStruct);
 
-  GPIO_InitStruct.Pin =  F3_Pin | F5_Pin | LED_STATUS_Pin | LED_POWER_Pin | CSYNC_Pin;
+  GPIO_InitStruct.Pin =  LED_STATUS_Pin | LED_POWER_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
--- printerFW/printer/tmp/Src/stm32f0xx_hal_msp.c
+++ printerFW/printer/tmp/Src/stm32f0xx_hal_msp.c
@@ -2,46 +2,46 @@
 /**
   ******************************************************************************
   * File Name          : stm32f0xx_hal_msp.c
-  * Description        : This file provides code for the MSP Initialization 
+  * Description        : This file provides code for the MSP Initialization
   *                      and de-Initialization codes.
   ******************************************************************************
   * This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * Copyright (c) 2019 STMicroelectronics International N.V. 
+  * Copyright (c) 2019 STMicroelectronics International N.V.
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitted, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice, 
+  * 1. Redistribution of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
+  * 4. This software, including modifications and/or derivative works of this
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
@@ -72,7 +72,7 @@ extern DMA_HandleTypeDef hdma_usart3_tx;
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN Define */
- 
+
 /* USER CODE END Define */
 
 /* Private macro -------------------------------------------------------------*/
@@ -98,7 +98,7 @@ extern DMA_HandleTypeDef hdma_usart3_tx;
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
-                        
+
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
                     /**
   * Initializes the Global MSP.
@@ -145,11 +145,11 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
   /* USER CODE END TIM1_MspInit 0 */
     /* Peripheral clock enable */
     __HAL_RCC_TIM1_CLK_ENABLE();
-  
+
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**TIM1 GPIO Configuration    
+    /**TIM1 GPIO Configuration
     PA8     ------> TIM1_CH1
-    PA9     ------> TIM1_CH2 
+    PA9     ------> TIM1_CH2
     */
     GPIO_InitStruct.Pin = ENCODER_B_Pin|ENCODER_A_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -181,14 +181,14 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
   /* USER CODE END TIM2_MspInit 0 */
     /* Peripheral clock enable */
     __HAL_RCC_TIM2_CLK_ENABLE();
-  
+
     /* TIM2 DMA Init */
     /* TIM2_CH1 Init */
     hdma_tim2_ch1.Instance = DMA1_Channel5;
     hdma_tim2_ch1.Init.Direction = DMA_MEMORY_TO_PERIPH;
     hdma_tim2_ch1.Init.PeriphInc = DMA_PINC_DISABLE;
     hdma_tim2_ch1.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_tim2_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_tim2_ch1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_tim2_ch1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
     hdma_tim2_ch1.Init.Mode = DMA_NORMAL;
     hdma_tim2_ch1.Init.Priority = DMA_PRIORITY_HIGH;
@@ -220,8 +220,8 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     hdma_tim2_ch3.Init.Direction = DMA_MEMORY_TO_PERIPH;
     hdma_tim2_ch3.Init.PeriphInc = DMA_PINC_DISABLE;
     hdma_tim2_ch3.Init.MemInc = DMA_MINC_ENABLE;
-    hdma_tim2_ch3.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    hdma_tim2_ch3.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_tim2_ch3.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_tim2_ch3.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
     hdma_tim2_ch3.Init.Mode = DMA_NORMAL;
     hdma_tim2_ch3.Init.Priority = DMA_PRIORITY_HIGH;
     if (HAL_DMA_Init(&hdma_tim2_ch3) != HAL_OK)
@@ -258,11 +258,11 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
   /* USER CODE BEGIN TIM3_MspPostInit 0 */
 
   /* USER CODE END TIM3_MspPostInit 0 */
-  
+
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**TIM3 GPIO Configuration    
+    /**TIM3 GPIO Configuration
     PB0     ------> TIM3_CH3
-    PB1     ------> TIM3_CH4 
+    PB1     ------> TIM3_CH4
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -294,10 +294,10 @@ void HAL_TIM_Encoder_MspDeInit(TIM_HandleTypeDef* htim_encoder)
   /* USER CODE END TIM1_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_TIM1_CLK_DISABLE();
-  
-    /**TIM1 GPIO Configuration    
+
+    /**TIM1 GPIO Configuration
     PA8     ------> TIM1_CH1
-    PA9     ------> TIM1_CH2 
+    PA9     ------> TIM1_CH2
     */
     HAL_GPIO_DeInit(GPIOA, ENCODER_B_Pin|ENCODER_A_Pin);
 
@@ -365,11 +365,11 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
   /* USER CODE END USART3_MspInit 0 */
     /* Peripheral clock enable */
     __HAL_RCC_USART3_CLK_ENABLE();
-  
+
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**USART3 GPIO Configuration    
+    /**USART3 GPIO Configuration
     PB10     ------> USART3_TX
-    PB11     ------> USART3_RX 
+    PB11     ------> USART3_RX
     */
     GPIO_InitStruct.Pin = IN_TX_Pin|IN_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -435,10 +435,10 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
   /* USER CODE END USART3_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_USART3_CLK_DISABLE();
-  
-    /**USART3 GPIO Configuration    
+
+    /**USART3 GPIO Configuration
     PB10     ------> USART3_TX
-    PB11     ------> USART3_RX 
+    PB11     ------> USART3_RX
     */
     HAL_GPIO_DeInit(GPIOB, IN_TX_Pin|IN_RX_Pin);
 
